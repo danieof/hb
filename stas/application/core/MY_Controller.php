1a819f2e7beafc323d97c40115cb1dec74e84f58
danieof_home <danieof@gmail.com>
2011-02-15 22:08:23 +0100

    stas - core

--- stas/application/core/MY_Controller.php
+++ stas/application/core/MY_Controller.php
@@ -5,9 +5,10 @@ require APPPATH . 'third_party/MX/Controller.php';
 class MY_Controller extends MX_Controller {
 	public function __construct() {
 		parent::__construct();
-        //$this->load->library('template');
+        $this->load->library('tank_auth');
+		$this->lang->load('tank_auth');
+        $this->load->config('tank_auth');
 	}
-
 }
 
 /* End of file MY_Controller.php */
--- stas/application/core/MY_Router.php
+++ stas/application/core/MY_Router.php
@@ -3,4 +3,8 @@
 /* load the MX_Router class */
 require APPPATH."third_party/MX/Router.php";
 
-class MY_Router extends MX_Router {}
+class MY_Router extends MX_Router {
+    public function  __construct() {
+        parent::__construct();
+    }
+}
