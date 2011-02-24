d369a69ffdfd846678b352647154b822e319e652
danieof_home <danieof@gmail.com>
2011-02-24 21:52:32 +0100

    drobne poprawki layout

--- harmonogramy/application/views/layouts/application.php
+++ harmonogramy/application/views/layouts/application.php
@@ -1,10 +1,12 @@
 <?php echo $this->template->block('header', 'partials/header'); ?>
 <?php
     echo anchor('uzytkownicy/lista_pracownicy/', 'Lista pracownikow') . '<br />';
-    echo anchor('pracownicy/dodaj', 'Dodaj pracownika') . '<br />';
+    echo anchor('pracownicy/edytuj', 'Dodaj pracownika') . '<br />';
     echo anchor('uzytkownicy/lista_harmonogramy/', 'Lista harmonogramow') . '<br />';
     echo anchor('uzytkownicy/lista_obowiazki/', 'Lista obowiazkow') . '<br />';
     echo anchor('uzytkownicy/lista_role/', 'Lista rol') . '<br />';
+    echo anchor('uzytkownicy/zaloguj/', 'Zaloguj') . '<br />';
+    echo anchor('uzytkownicy/wyloguj/', 'Wyloguj') . '<br />';
 
     echo '<br /><br />';
 ?>
