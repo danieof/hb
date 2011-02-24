44c2e00d1f8900515985f588a1de5b162d476d2b
danieof_home <danieof@gmail.com>
2011-02-24 21:52:09 +0100

    zaleznosci - puste

---
+++ harmonogramy/application/modules/zaleznosci/controllers/zaleznosci.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class rename extends MY_Controller {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function index() {
+        
+	}
+}
+
+/* End of file rename.php */
+/* Location: ./application/modules/rename/controllers/rename.php */
